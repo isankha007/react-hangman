7cd8178ce3e1290c18bc467dcd860264ce9d38b4
sankhadeepchatterjeee <sankhadeep2007@gmail.com>
2024-09-15 12:29:07 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import words from "./wordList.json"
+import { HangmanDrawing } from "./components/HangmanDrawing"
+import { HangmanWord } from "./components/HangmaWord"
+import { Keyboard } from "./components/Keyboard"
+
+function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+
+  console.log(wordToGuess);
+  return (
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}>
+      <div style={{
+        fontSize:"2rem",
+        textAlign:"center"
+        }}>
+          Lose win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,16 @@
+export function HangmanDrawing(){
+    return (
+        <div style={{position:"relative"}}>
+            <div style={{
+                height:"400px",
+                width:"10px",
+                background:"black",
+                marginLeft:"120px"
+            }}/>
+            <div style={{height: "10px",
+                width:"250px",
+                background:"black"
+            }}></div>
+        </div>
+    )
+}
