59af914b7e04b851485b7112d51f189f3ea21246
sankhadeepchatterjeee <sankhadeep2007@gmail.com>
2024-09-15 13:47:04 +0530

    add arm leg

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -25,11 +25,71 @@ const BODY=(
     }/>
 )
 
+const RIGHT_ARM=(
+    <div style={
+       {width: "100px",
+        height:"10px",
+        background:" black",
+        position:"absolute",
+        top:"150px",
+        right:"-100px",
+        rotate:"-30deg",
+        transformOrigin:"left bottom",
+       }
+    }/>
+)
+
+const LEFT_ARM=(
+    <div style={
+       {width: "100px",
+        height:"10px",
+        background:" black",
+        position:"absolute",
+        top:"150px",
+        right:"10px",
+        rotate:"30deg",
+        transformOrigin:"right bottom",
+       }
+    }/>
+)
+
+const RIGHT_LEG=(
+    <div style={
+       {width: "100px",
+        height:"10px",
+        background:" black",
+        position:"absolute",
+        top:"210px",
+        right:"-90px",
+        rotate:"60deg",
+        transformOrigin:"left bottom",
+       }
+    }/>
+)
+
+const LEFT_LEG=(
+    <div style={
+       {width: "100px",
+        height:"10px",
+        background:" black",
+        position:"absolute",
+        top:"210px",
+        right:0,
+        rotate:"-60deg",
+        transformOrigin:"right bottom",
+       }
+    }/>
+)
+
 export function HangmanDrawing(){
     return (
         <div style={{position:"relative"}}>
             {HEAD}
             {BODY}
+            {RIGHT_ARM}
+            {LEFT_ARM}
+            {RIGHT_LEG}
+            {LEFT_LEG}
              <div style={{
                 height:"50px",
                 width:"10px",
